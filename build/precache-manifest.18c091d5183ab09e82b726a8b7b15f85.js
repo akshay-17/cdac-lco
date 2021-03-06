d96c12764c1038e84b2e08b0a6fab9b92121d4ef
AkshayKhandelwal <akshaykhandelwal06@gmail.com>
2021-03-07 00:41:20 +0530

    first commit

---
+++ build/precache-manifest.18c091d5183ab09e82b726a8b7b15f85.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc0c487401db92ccc1b3c8d468066cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b76c46414e2852be0b1c",
+    "url": "/static/css/main.d6bc74ea.chunk.css"
+  },
+  {
+    "revision": "07428da1e9275ea1aaa2",
+    "url": "/static/js/2.ca881663.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.ca881663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76c46414e2852be0b1c",
+    "url": "/static/js/main.c0f8421e.chunk.js"
+  },
+  {
+    "revision": "2b55614ca07f93359093",
+    "url": "/static/js/runtime-main.bfd8333c.js"
+  },
+  {
+    "revision": "e8f383b495b55bf8f54ef3fb731b7fde",
+    "url": "/static/media/addcart.e8f383b4.png"
+  },
+  {
+    "revision": "a5074e4f3fdd3f2c070ca17e536e798d",
+    "url": "/static/media/contact.a5074e4f.png"
+  },
+  {
+    "revision": "26664a8bf62908a43968b952fcdf8b3c",
+    "url": "/static/media/lcomerchlogo.26664a8b.png"
+  },
+  {
+    "revision": "a70b45d0c48294ce0efe9b5e7a5087ff",
+    "url": "/static/media/removecart.a70b45d0.png"
+  },
+  {
+    "revision": "49018a330e721049951ec6a19dbf1267",
+    "url": "/static/media/userdashboard.49018a33.jpg"
+  },
+  {
+    "revision": "3749303936a6c0d0629b8bc4ecc928bf",
+    "url": "/static/media/vector1.37493039.png"
+  }
+]);
